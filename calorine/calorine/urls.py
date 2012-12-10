b8b16c4f179f9bb7443426e228d573c5d59a385f
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2012-12-10 18:17:05 +0100

    Protect som urls

--- calorine/calorine/urls.py
+++ calorine/calorine/urls.py
@@ -1,19 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from calorine.caro.views import SongList, PlayList, Profile, LogList
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^accounts/', include('registration.urls')),
                        url(r'^accounts/profile/$', Profile.as_view()),
-                       url(r'^songs/$', SongList.as_view()),
-                       url(r'^logs$', LogList.as_view()),
+                       url(r'^songs/$', login_required(SongList.as_view())),
+                       url(r'^logs$', login_required(LogList.as_view())),
                        url(r'^playlist/add/(?P<song_id>\d+)$', 'calorine.caro.views.pladd'),
                        url(r'^playlist/inc/(?P<pk>\d+)$', 'calorine.caro.views.pllike'),
                        url(r'^playlist/dec/(?P<pk>\d+)$', 'calorine.caro.views.pldislike'),
-                       url(r'^$', PlayList.as_view(), name="playlist"),
+                       url(r'^$', login_required(PlayList.as_view(), name="playlist")),
                        url(r'^admin/', include(admin.site.urls)),
                        )
 # Examples:
@@ -22,4 +22,3 @@ urlpatterns = patterns('',
 # Uncomment the admin/doc line below to enable admin documentation:
 # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 # Uncomment the next line to enable the admin:
-
