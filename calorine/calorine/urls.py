aa4f0665c8fa7c62af0f233322b173518bf90783
Yohann Gabory <yohann@gabory.fr>
2012-12-10 14:04:48 +0100

    ajout d'une vue générique pour song

--- calorine/calorine/urls.py
+++ calorine/calorine/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.shortcuts import redirect
-
+from calorine.caro.views import SongList
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,10 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns('',    
                        url(r'^accounts/', include('registration.urls')),
                        url(r'^accounts/profile/$', 'calorine.caro.views.profile'),
-                       url(r'^songs/$', 'calorine.caro.views.songs'),
+                       url(r'^songs/$', SongList.as_view()),
                        url(r'^logs$', 'calorine.caro.views.logs'),
                        url(r'^playlist/add/(?P<song_id>\d+)$', 'calorine.caro.views.pladd'),
-                       url(r'^$', 'calorine.caro.views.playlist')
+                       url(r'^$', 'calorine.caro.views.playlist'),
                        )
 # Examples:
 # url(r'^$', 'calorine.views.home', name='home'),
