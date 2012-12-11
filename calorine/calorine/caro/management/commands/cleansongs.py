68914aecb86b509eec38a7c4a358b6ae04d72cba
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2012-12-11 11:33:21 +0100

    Improve output message

--- calorine/calorine/caro/management/commands/cleansongs.py
+++ calorine/calorine/caro/management/commands/cleansongs.py
@@ -6,9 +6,10 @@ class Command(BaseCommand):
     help = 'Remove from db unonw on disk song'
 
     def handle(self, *args, **options):
-        try:
+        count = Song.objects.filter(score=-1000).count()
+        try:            
             Song.objects.filter(score=-1000).delete()
         except Song.DoesNotExist:
             raise CommandError('Unable to remove song from db')
 
-        self.stdout.write('Successfully clean database\n')
+        self.stdout.write('Successfully remove %d songs from database\n' % count)
