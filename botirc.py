d4197a6c741d18b8790a54d8311d1b1b28bb1766
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2012-12-11 13:09:56 +0100

    Add the killing feature

--- botirc.py
+++ botirc.py
@@ -7,6 +7,7 @@ import irclib
 import ircbot
 import syslog
 import socket
+import subprocess
 from threading import Thread
 from botcommon import OutputManager
 from random import random
@@ -16,7 +17,8 @@ from optparse import OptionParser
 debian package python-irclib
 """
 
-class BotModeration(ircbot.SingleServerIRCBot):
+class BotCalorine(ircbot.SingleServerIRCBot):
+
     def __init__(self, nick, chan, url):
         """
         Constructeur qui pourrait prendre des paramètres dans un "vrai" programme.
@@ -32,11 +34,12 @@ class BotModeration(ircbot.SingleServerIRCBot):
         self.nick = nick
         self.url = url
 
-    def on_welcome(self, serv, ev):
+    def on_welcome(self, serv, event):
         """
         Méthode appelée une fois connecté et identifié.
         Notez qu'on ne peut rejoindre les canaux auparavant.
         """
+        syslog.syslog("irc: join %s" % self.chan)
         serv.join(self.chan)
         wmsg = ["yo les baiseaux", "salut les bouseux",
                 "coucou les filles", "coucou tout le monde",
@@ -46,22 +49,22 @@ class BotModeration(ircbot.SingleServerIRCBot):
         rand = int(random() * 10)
         serv.privmsg(self.chan, wmsg[rand])
 
-    def on_kick(self, serv, ev):
+    def on_kick(self, serv, event):
         """
         auto rejoin on kick
         """
         serv.join(self.chan)
         serv.privmsg(self.chan, "qui c'est qui qui ma kické, kiki ?")
 
-    def on_pubmsg(self, serv, ev):
+    def on_pubmsg(self, serv, event):
         """
         Méthode appelée à la réception d'un message
         """
         # Il n'est pas indispensable de passer par des variables
         # Ici elles permettent de clarifier le tout.
-        auteur = irclib.nm_to_n(ev.source())
-        canal = ev.target()
-        message = ev.arguments()[0].lower() # Les insultes sont écrites en minuscules.
+        auteur = irclib.nm_to_n(event.source())
+        canal = event.target()
+        message = event.arguments()[0].lower() # Les insultes sont écrites en minuscules.
 
         insultes = ["con ", "pute", "connard", "salope", "salop", "crétin"]
 
@@ -71,22 +74,13 @@ class BotModeration(ircbot.SingleServerIRCBot):
                 break
 
         if message.startswith(self.nick):
+            self.action(message)
 
-            if message == "%s: cassos" % self.nick:
-                self.queue.stop()
-                self.speak("ok je comprends")
-                self.inputthread.go_on = False
-                serv.disconnect("Au revoir, comme aurait dit VGE")
-                sys.exit(0)
-            else:
-                self.speak("tu aimes la musique, ecoute %s" % self.url)
-
-    def on_join(self, serv, ev):
+    def on_join(self, serv, event):
         """
         When someone join
         """
-        auteur = irclib.nm_to_n(ev.source())
-        canal = ev.target()
+        auteur = irclib.nm_to_n(event.source())
 
         hello = ["salut", "coucou", "bonjour",
                  "yo", "bisous", "kiss",
@@ -101,6 +95,51 @@ class BotModeration(ircbot.SingleServerIRCBot):
         speak on chan
         """
         self.queue.send(message, self.chan)
+
+    def action(self, message):
+        msg = message.lower()
+        if message.startswith(self.nick):
+            if message == "%s: cassos" % self.nick:
+                self.queue.stop()
+                self.speak("ok je comprends")
+                self.inputthread.go_on = False
+                serv.disconnect("Au revoir, comme aurait dit VGE")
+                sys.exit(0)
+            elif msg.startswith("%s: je kiffe " % self.nick):
+                self.addpl(message)
+            elif msg.startswith("%s: j'aime " % self.nick):
+                self.addpl(message)
+            elif msg.startswith("%s: donne moi du " % self.nick):
+                self.addpl(message)
+            else:
+                self.speak("tu aimes la musique, ecoute %s" % self.url)
+
+    def addpl(self, message):
+        msg = message.lower()
+        pl_prefix = ["je kiffe", "j'aime", "je like", "donne moi du"]
+        manage = os.path.join(os.path.dirname(__file__),
+                              'calorine',
+                              'manage.py')
+        syslog.syslog("run %s" % manage)
+        if 'britney' in msg:
+            self.speak("euh, ça non je ne peux pas, Britney ça fait saigner les oreilles, désolé")
+            return 0
+
+        if ';' in msg:
+            self.speak("hum j'aime pas trop les points-virgules")
+            return 0
+
+        if '&' in msg:
+            self.speak("je n'aime pas les esperluettes d'abord")
+            return 0
+
+        for plp in pl_prefix:
+            if msg.startswith("%s: %s" % (self.nick, plp)):
+                index = len(self.nick) + 2 + len(plp) + 1
+                lookup = message[index:]
+                syslog.syslog("action: lookup %s " % lookup)
+                res = subprocess.check_output([manage, 'lookup_add_playlist', lookup])
+                self.speak(res.rstrip())
 
 
 class sockinput(Thread):
@@ -127,7 +166,7 @@ class sockinput(Thread):
         while self.go_on:
             conn, addr = self.server.accept()
 
-            while True: 
+            while True:
                 data = conn.recv( 1024 )
                 if not data:
                     break
@@ -141,13 +180,13 @@ def readopts():
     """
     parser = OptionParser()
     parser.add_option("--dbname", action="store", type="string", dest="dbname", default=None)
-    
+
     parser.add_option("-c", "--chan",
                       action="store",
                       type="string",
                       dest="chan",
                       default=None)
-    
+
     (options, args) = parser.parse_args()
     return options
 
@@ -158,9 +197,9 @@ def main():
     nickname = 'calorine'
     url = 'http://calorine.quiedeville.org:8042/chezleo'
     options = readopts()
-    bot = BotModeration(nickname, "#%s" % options.chan, url)
+    bot = BotCalorine(nickname, "#%s" % options.chan, url)
     bot.start()
 
 if __name__ == "__main__":
     main()
-    
+
