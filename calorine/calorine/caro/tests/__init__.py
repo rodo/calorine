b2ca9216e73aea215e23e30e0f97d19a036fbd7c
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2012-12-12 23:17:23 +0100

    Add test on commands

--- calorine/calorine/caro/tests/__init__.py
+++ calorine/calorine/caro/tests/__init__.py
@@ -6,3 +6,4 @@ from calorine.caro.tests.logs import LogsTests
 from calorine.caro.tests.cache import CachesTests
 from calorine.caro.tests.vote import VoteTests
 from calorine.caro.tests.urls import UrlsTests
+from calorine.caro.tests.command import CommandTests
---
+++ calorine/calorine/caro/tests/command.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012 Rodolphe Quiédeville <rodolphe@quiedeville.org>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Unit tests for Vote
+
+"""
+from django.contrib.auth.models import User
+from django.test import TestCase
+from calorine.caro.models import Song
+from django.core.management import call_command
+
+class CommandTests(TestCase):  # pylint: disable-msg=R0904
+    """
+    The profile view
+
+    """
+    def setUp(self):
+        """
+        Init
+        """
+        self.user = User.objects.create_user('admin_search',
+                                             'admin_search@bar.com',
+                                             'admintest')
+
+    def test_cleansongs(self):
+        """
+        cleansongs manage command
+        """
+        song = Song.objects.create(artist='Van Morrison',
+                                   album='The Healing Game',
+                                   title='Sometimes We Cry',
+                                   genre='Blues',
+                                   score=-1000,
+                                   family=0,
+                                   global_score=0)
+
+        before = Song.objects.filter(score=-1000).count()
+
+        call_command('cleansongs')
+
+        after = Song.objects.filter(score=-1000).count()
+        
+        self.assertTrue(before > 0)
+        self.assertEqual(after, 0)
