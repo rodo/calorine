7fea7f04c88d83a349a6b011a0524c7aabd52949
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2012-12-19 11:02:46 +0100

    Fix error in resgister.inclusion

--- calorine/calorine/caro/templatetags/lastfm.py
+++ calorine/calorine/caro/templatetags/lastfm.py
@@ -1,8 +1,25 @@
+# -*- coding: utf-8 -*-  pylint: disable-msg=R0801
+#
+# Copyright (c) 2012 Rodolphe Quiédeville <rodolphe@quiedeville.org>
+# Copyright (c) 2012 Yohann Gabory <yohann@gabory.fr>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from django import template
 from calorine.utils.lastfm import get_picture
 from django.core.cache import cache
 from django.template.defaultfilters import slugify
-
 register = template.Library()
 
 
@@ -24,4 +41,4 @@ def picture(song):
         'picture': pict
     }
 
-register.inclusion_tag('picture.html')(pict)
+register.inclusion_tag('picture.html')(picture)
