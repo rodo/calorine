2769b7719af05fd582543d9c7d6f34c2c2c1c092
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-01-07 21:17:06 +0100

    Remove unused imports

--- calorine/calorine/caro/tests/upload.py
+++ calorine/calorine/caro/tests/upload.py
@@ -21,9 +21,7 @@ Unit tests for Upload
 """
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.test import Client
 from calorine.caro.models import Upload
-import os
 
 
 class UploadTests(TestCase):  # pylint: disable-msg=R0904
