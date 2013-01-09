403d3364279b03b0644b40d298c918be0182920a
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-01-09 21:08:26 +0100

    Sync

--- calorine/calorine/caro/tests/command.py
+++ calorine/calorine/caro/tests/command.py
@@ -258,6 +258,28 @@ class CommandTests(TestCase):  # pylint: disable-msg=R0904
         self.assertEqual(before, 0)
         self.assertEqual(after, 1)
 
+    def test_lookup_add_playlist_twice(self):
+        """
+        Management command lookup_add_playlist
+
+        Lookup for a song and add them in playlist if found
+        """
+        PlaylistEntry.objects.all().delete()
+
+        Song.objects.create(artist='Fatoumata Diawara',
+                            album='Kanou',
+                            title='Nayan',
+                            genre='Folk Wassoulou',
+                            score=0,
+                            family=0,
+                            global_score=0)
+
+        call_command('lookup_add_playlist', 'Nayan')
+        # call twice
+        call_command('lookup_add_playlist', 'Nayan')
+
+        self.assertEqual(PlaylistEntry.objects.all().count(), 1)
+
     def test_lookup_add_playlist2songsmatch(self):
         """
         Management command lookup_add_playlist
