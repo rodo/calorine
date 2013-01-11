51942d5b24d13c02d7008c310eeadd9f2e5f6ae1
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-01-11 20:27:50 +0100

    Add sample

--- calorine/calorine/caro/tests/httpserver.py
+++ calorine/calorine/caro/tests/httpserver.py
@@ -62,12 +62,52 @@ class LastFMHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     """
     Basic handler, serve a basic JSON answer
     """
+    
+    sample = {"track": 
+              {"id":"1019817",
+               "name":"Believe",
+               "mbid":"64e9df18-0ffb-4fda-bac8-6b52b926c7ec",
+               "url":"http:\/\/www.last.fm\/music\/Cher\/_\/Believe",
+               "duration":"222000",
+               "streamable":{"#text":"1","fulltrack":"0"},
+               "listeners":"287841",
+               "playcount":"1208728",
+               "artist":{"name":"Cher",
+                         "mbid":"bfcc6d75-a6a5-4bc6-8282-47aec8531818",
+                         "url":"http:\/\/www.last.fm\/music\/Cher"},
+               "album":{"artist":"Cher",
+                        "title":"The Very Best of Cher",
+                        "mbid":"5df00011-6e57-4837-99ae-61f398481db5",
+                        "url":"http:\/\/www.last.fm\/music\/Cher\/The+Very+Best+of+Cher",
+                        "image":[{"#text":"http:\/\/userserve-ak.last.fm\/serve\/64s\/71997588.png",
+                                  "size":"small"},
+                                 {"#text":"http:\/\/userserve-ak.last.fm\/serve\/126\/71997588.png",
+                                  "size":"medium"},
+                                 {"#text":"http:\/\/userserve-ak.last.fm\/serve\/174s\/71997588.png",
+                                  "size":"large"},
+                                 {"#text":"http:\/\/userserve-ak.last.fm\/serve\/300x300\/71997588.png",
+                                  "size":"extralarge"}],
+                        "@attr":{"position":"1"}},
+               "toptags":{"tag":[{"name":"pop",
+                                  "url":"http:\/\/www.last.fm\/tag\/pop"},
+                                 {"name":"dance",
+                                  "url":"http:\/\/www.last.fm\/tag\/dance"},
+                                 {"name":"90s",
+                                  "url":"http:\/\/www.last.fm\/tag\/90s"},
+                                 {"name":"cher",
+                                  "url":"http:\/\/www.last.fm\/tag\/cher"},
+                                 {"name":"female vocalists","url":"http:\/\/www.last.fm\/tag\/female%20vocalists"}]},
+               "wiki":{"published":"Tue, 17 Nov 2009 15:02:18 +0000",
+                       "summary":"blah blah"}
+               }
+              }
+
     def do_GET(self):
         """Respond to a GET request."""
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
-        self.wfile.write('{"state": "done"}')
+        self.wfile.write('%s' % sample)
 
 
 class LastFMServer(threading.Thread):
@@ -81,7 +121,7 @@ class LastFMServer(threading.Thread):
         while not connected and self.port < 2048:
             try:
                 self.httpd = SocketServer.TCPServer(("", self.port),
-                                                    LastFM)
+                                                    LastFMHandler)
                 self.httpd.timeout = 30
                 connected = True
             except:
